ea0ec851987fa0d7fe43519d3d427f5bfb366b37
Nicolas Anzoategui <nanzoate@gmail.com>
2024-09-06 17:23:35 +0200

    clean up comments and improve the logic for rendering the image of each business

--- src/pages/BusinessList.jsx
+++ src/pages/BusinessList.jsx
@@ -2,13 +2,19 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import businessService from "../services/business.services";
 
+import defaultImageArcade from "/defaultImageArcade.jpg";
+import defaultImageBookstore from "/defaultImageBookstore.webp";
+import defaultImageCoffeeshop from "/defaultImageCoffeeshop.png";
+import defaultImageFair from "/defaultImageFair.jpeg";
+import defaultImageRestaurant from "/defaultImageRestaurant.jpg";
+
 function BusinessList () {
 
     const [ businesses, setBusinesses ] = useState(null);
     const [ error, setError ] = useState(null);
     const [ loading, setLoading ] = useState(true);
     const [query, setQuery] = useState("");
-    // const [ filteredBusinesses, setFilteredBusinesses ] = useState(businesses);
+
     let filteredBusinesses;
 
 
@@ -30,13 +36,7 @@ function BusinessList () {
 
     if(Array.isArray(businesses)) {
         filteredBusinesses = businesses.filter( (business) => {
-            // return  (
-            //         business.name.toLowerCase().includes(query.toLowerCase())
-            //         || business.category.toLowerCase().includes(query.toLowerCase())
-            //         || business.location.toLowerCase().includes(query.toLowerCase())
-            //         || business.description.toLowerCase().includes(query.toLowerCase())
-            // )   
-
+           
             if(business.name && business.name.toLowerCase().includes(query.toLowerCase())){return true} else
             if(business.category && business.category.toLowerCase().includes(query.toLowerCase())){return true} else
             if(business.location && business.location.toLowerCase().includes(query.toLowerCase())){return true} else
@@ -44,29 +44,53 @@ function BusinessList () {
         });
     }
     
+    
+    const getImg = (business) => {
+        if(business.imageURL) {
+            return business.imageURL;
+        } else  switch (business.category) {
+                case "restaurant":
+                    return defaultImageRestaurant;
+                    break;
+                case "bookstore":
+                    return defaultImageBookstore;
+                    break;
+                case "coffeeshop":
+                    return defaultImageCoffeeshop;
+                    break;
+                case "arcade":
+                    return defaultImageArcade;
+                    break;
+                case "fair":
+                    return defaultImageFair; 
+                    break;
+            }
+    }
 
     return (
-        <>
-            <div>
-                Search
-                <input value={query} type="search" onChange={e => setQuery(e.target.value)}/>
+        <div className="mx-5 min-h-full">
+            <div className="flex-wrap py-5 px-5 bg-slate-300 max-w-xs my-5 mx-2 rounded-md">
+                <span>Search</span>
+                <input className="mx-5" value={query} type="search" onChange={e => setQuery(e.target.value)}/>
             </div>
 
             <div className="flex">
                 {Array.isArray(filteredBusinesses) && filteredBusinesses.length > 0 
                 ? filteredBusinesses.map( (business) => {
+                    
                     return (
-                    <div className="card flex hover:bg-green-200 m-1" key={business._id}>
+                    <div className="card flex hover:bg-green-200 m-2 w-1/4 h-96 justify-center" key={business._id}>
                         <Link to={`/businesses/${business._id}`}>
                             <h3>{business.name}</h3>
-                            <img src={business.imageUrl} alt="business image" />
+                            <img className="min-w-max" src={getImg(business)} alt="business image" />
                             <p>{business.location}</p>
+                            <p>{business.category}</p>
                         </Link> 
                     </div>) 
                 }) 
                 : <p>No businesses found.</p>}
             </div>
-        </>
+        </div>
     )
 }
 
