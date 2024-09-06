1561189d84cd7d896f7943cee6431bd64a3c1ab7
Nicolas Anzoategui <nanzoate@gmail.com>
2024-09-06 17:21:34 +0200

    add styling to nav bar, footer and a background image to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 function HomePage () {
     return (
         <>
-            <div className="flex-row flex-grow min-h-full bg-red-100">
-                <h1 className=" text-center my-5 text-2xl">Welcome to Local Findie</h1>
+            <div className="flex-row min-h-full bg-pack-train bg-no-repeat bg-cover ">
+                <h1 className=" text-center py-5 text-2xl">Welcome to Local Findie</h1>
 
                 <h3 className="indent-10 mx-20 text-lg leading-loose">Discover the best local businesses around you! Whether you're looking for a cozy coffee shop, a trusted mechanic, or the latest boutique shop, Local Findie has you covered. Browse reviews, explore hidden gems, and support your local community with just a few clicks. Start your journey today!</h3>
             </div>
+            
         </>
     )
 }
