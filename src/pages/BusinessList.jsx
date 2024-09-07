a4abaed5c9455c43df405ea22919e0a80ea3b11a
Nicolas Anzoategui <nanzoate@gmail.com>
2024-09-07 15:58:20 +0200

    fix search box to navbar, apply grid to display the cards.

--- src/pages/BusinessList.jsx
+++ src/pages/BusinessList.jsx
@@ -63,29 +63,32 @@ function BusinessList () {
     }
 
     return (
-        <div className="flex-col mx-5 h-full w-full pt-14">
-            <div className="flex py-3 px-5 bg-slate-300 max-w-xs h-12 my-5 mx-2 rounded-md">
+        <div className="min-w-full">
+            <div className="fixed top-0 left-7 max-w-xs my-4 z-20">
                 <span>Search</span>
                 <input className="mx-5" value={query} type="search" onChange={e => setQuery(e.target.value)}/>
             </div>
-            
-            <div className="flex">
-                {Array.isArray(filteredBusinesses) && filteredBusinesses.length > 0 
-                ? filteredBusinesses.map( (business) => {
-                    
-                    return (
-                    <div className="card box-border hover:bg-green-200 m-2 min-h-96 min-w-72 justify-center" key={business._id}>
-                        <Link to={`/businesses/${business._id}`}>
-                            <h3 className="my-3">{business.name}</h3>
-                            <img className="min-w-60 min-h-48 object-cover" src={getImg(business)} alt="business image" />
-                            <p className="mt-7">{business.category.slice(0,1).toUpperCase() + business.category.slice(1)}</p>
-                            <p className="my-3">{business.location}</p>
-                        </Link> 
-                    </div>) 
-                }) 
-                : <p>No businesses found.</p>}
+
+            <div className="pt-20 pb-12 mx-5">
+                <div className="grid grid-cols-4 gap-5 justify-center">
+                    {Array.isArray(filteredBusinesses) && filteredBusinesses.length > 0 
+                    ? filteredBusinesses.map( (business) => {
+                        
+                        return (
+                            <div className="card box-border hover:bg-green-200 min-h-96 max-h-96 justify-center" key={business._id}>
+                                <Link to={`/businesses/${business._id}`}>
+                                    <h3 className="mb-5">{business.name}</h3>
+                                    <img className="min-w-full min-h-48 max-h-48 object-fill" src={getImg(business)} alt="business image" />
+                                    <p className="mt-7">{business.category.slice(0,1).toUpperCase() + business.category.slice(1)}</p>
+                                    <p className="my-3">{business.location}</p>
+                                </Link> 
+                            </div>) 
+                        }) 
+                    : <p>No businesses found.</p>}
+                </div>
             </div>
         </div>
+        
     )
 }
 
