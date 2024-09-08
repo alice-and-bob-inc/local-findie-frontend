661f92735eaaf8df508cc2743906e0dd507e8f5e
Nicolas Anzoategui <nanzoate@gmail.com>
2024-09-08 20:36:12 +0200

    change home page bg image, favicon and add styling to text

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,19 @@
-function HomePage () {
+function HomePage() {
     return (
-        <>
-            <div className="flex justify-between min-h-full bg-home-page bg-no-repeat bg-cover bg-[center_bottom_-333px] relative">
-                {/* <h1 className="pl-10 py-4 text-2xl z-40 relative">Welcome to Local Findie</h1> */}
+        <div className="relative min-h-full bg-home-page bg-no-repeat bg-contain bg-[center_bottom_10px]">
 
-                <h3 className="px-20 pt-24 max-w-md text-lg leading-loose font-semibold text-gray-800">Welcome to Local Findie. Discover the best local businesses around you! </h3>
-                <h3 className="px-20 pt-32 max-w-md text-lg leading-loose font-semibold text-gray-800">Browse reviews, explore hidden gems, and support your local community with just a few clicks!</h3>
-            </div>
+            <div className="absolute inset-0 bg-white opacity-0"></div>
             
-        </>
-    )
-}
+            <div className="top-28 relative z-10 flex justify-around">
+                <h3 className="ml-2 max-w-sm text-xl leading-loose font-semibold text-gray-700">
+                    Discover the best local businesses around you!
+                </h3>
+                <h3 className="mt-10 pr-20 max-w-2xl text-xl leading-loose font-semibold text-gray-700">
+                    Browse reviews, explore hidden gems, and support your local community with just a few clicks!
+                </h3>
+            </div>
+        </div>
+    );
+} 
 
 export default HomePage;
