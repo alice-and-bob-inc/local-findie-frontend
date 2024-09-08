5ea85f9da233da9b4eb8ea7d782ef46d83fe70e0
Nicolas Anzoategui <nanzoate@gmail.com>
2024-09-08 02:57:51 +0200

    add favicon. add hover effect to footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
     return (
       <footer className="bg-green-300 text-center p-1 fixed bottom-0 w-screen z-20 border border-green-400">
-        <a href="https://github.com/alice-and-bob-inc/local-findie-frontend" target="_blank">
+        <a className="hover:text-white" href="https://github.com/alice-and-bob-inc/local-findie-frontend" target="_blank">
           View this project on GitHub
         </a>
       </footer>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 function HomePage () {
     return (
         <>
-            <div className="flex justify-between min-h-full bg-home-page bg-no-repeat bg-cover bg-center">
+            <div className="flex justify-between min-h-full bg-home-page bg-no-repeat bg-cover bg-[center_bottom_-333px] relative">
                 {/* <h1 className="pl-10 py-4 text-2xl z-40 relative">Welcome to Local Findie</h1> */}
 
-                <h3 className="px-20 pt-24 max-w-md text-lg leading-loose">Welcome to Local Findie. Discover the best local businesses around you! </h3>
-                <h3 className="px-20 pt-32 max-w-md text-lg leading-loose">Browse reviews, explore hidden gems, and support your local community with just a few clicks!</h3>
+                <h3 className="px-20 pt-24 max-w-md text-lg leading-loose font-semibold text-gray-800">Welcome to Local Findie. Discover the best local businesses around you! </h3>
+                <h3 className="px-20 pt-32 max-w-md text-lg leading-loose font-semibold text-gray-800">Browse reviews, explore hidden gems, and support your local community with just a few clicks!</h3>
             </div>
             
         </>
