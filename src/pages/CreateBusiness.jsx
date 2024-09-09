26d403a69fb46876c67ca4dc31f629cd4be590c7
Nicolas Anzoategui <nanzoate@gmail.com>
2024-09-09 12:54:59 +0200

    add basic styling to CreateBusiness page

--- src/pages/CreateBusiness.jsx
+++ src/pages/CreateBusiness.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import businessService from "../services/business.services";
 
 function CreateBusiness() {
-
   const [name, setName] = useState("");
   const [imageURL, setImageURL] = useState("");
   const [description, setDescription] = useState("");
@@ -27,19 +26,27 @@ function CreateBusiness() {
     sunTill: "",
   });
 
-
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const requestBody = { name, imageURL, description, location, category, foundedYear, openingHours };
+    const requestBody = {
+      name,
+      imageURL,
+      description,
+      location,
+      category,
+      foundedYear,
+      openingHours,
+    };
 
-    businessService.createBusiness(requestBody)
-        .then((response) => {
-            navigate("/businesses");
-        })
-        .catch((error) => console.log(error));
+    businessService
+      .createBusiness(requestBody)
+      .then((response) => {
+        navigate("/businesses");
+      })
+      .catch((error) => console.log(error));
   };
 
   const handleOpeningHoursChange = (day, type, value) => {
@@ -52,120 +59,128 @@ function CreateBusiness() {
   const renderOpeningHours = () => {
     const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
     return days.map((day) => (
-      <div key={day}>
+      <div key={day} className="flex-col">
         <label>{day.charAt(0).toUpperCase() + day.slice(1)}:</label>
-        <label>From:
+        <label className="p-2">
+          From:
           <input
             type="time"
             value={openingHours[`${day}From`]}
-            onChange={(e) => handleOpeningHoursChange(day, "From", e.target.value)}
+            onChange={(e) =>
+              handleOpeningHoursChange(day, "From", e.target.value)
+            }
           />
         </label>
-        <label>Till:
+        <label className="p-2">
+          Till:
           <input
             type="time"
             value={openingHours[`${day}Till`]}
-            onChange={(e) => handleOpeningHoursChange(day, "Till", e.target.value)}
+            onChange={(e) =>
+              handleOpeningHoursChange(day, "Till", e.target.value)
+            }
           />
         </label>
-        <hr />
       </div>
     ));
   };
 
-
   return (
-    <div className="pt-20 ml-5">
-      <h3>Add Business</h3>
+    <div className="min-h-full pt-20 pb-12">
+      <div className="card mx-auto max-w-lg">
+        <h3 className="mb-5 text-lg">Add Business</h3>
 
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            placeholder="Il Gelatto"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </label>
-        <hr />
+        <form className="flex-col space-y-4" onSubmit={handleSubmit}>
+          <label className="flex-col">
+            Name
+            <input
+              className="block"
+              type="text"
+              name="name"
+              placeholder="Il Gelatto"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </label>
 
-        <label>
-          Image URL
-          <input
-            type="url"
-            name="image"
-            placeholder="https://example.com/path/to/image.jpg"
-            value={imageURL}
-            onChange={(e) => setImageURL(e.target.value)}
-          />
-        </label>
-        <hr />
+          <label className="flex-col">
+            Image URL
+            <input
+              className="block"
+              type="url"
+              name="image"
+              placeholder="https://example.com/path/to/image.jpg"
+              value={imageURL}
+              onChange={(e) => setImageURL(e.target.value)}
+            />
+          </label>
 
-        <label>
-          Description
-          <textarea
-            name="description"
-            placeholder="Il Gelatto is the coolest spot for..."
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </label>
-        <hr />
+          <label className="flex-col">
+            Description
+            <textarea
+              className="block"
+              name="description"
+              placeholder="Il Gelatto is the coolest spot for..."
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </label>
 
-        <label>
-          Location
-          <input
-            type="text"
-            name="location"
-            placeholder="Amsterdam"
-            min={1}
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-            required
-          />
-        </label>
-        <hr />
+          <label className="flex-col">
+            Location
+            <input
+              className="block"
+              type="text"
+              name="location"
+              placeholder="Amsterdam"
+              min={1}
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              required
+            />
+          </label>
 
-        <label>
-          Category
-          <select
-            name="category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            required
-          >
-            <option value="" disabled>
-              Select a category
-            </option>
-            <option value="restaurant">Restaurant</option>
-            <option value="bookstore">Bookstore</option>
-            <option value="coffeeshop">Coffee Shop</option>
-            <option value="arcade">Arcade</option>
-            <option value="fair">Fair</option>
-          </select>
-        </label>
-        <hr />
+          <label className="flex-col">
+            Category
+            <select
+              className="block"
+              name="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              required
+            >
+              <option value="" disabled>
+                Select a category
+              </option>
+              <option value="restaurant">Restaurant</option>
+              <option value="bookstore">Bookstore</option>
+              <option value="coffeeshop">Coffee Shop</option>
+              <option value="arcade">Arcade</option>
+              <option value="fair">Fair</option>
+            </select>
+          </label>
 
-        <label>
-          Founded Year
-          <input
-            type="number"
-            name="foundedYear"
-            placeholder="2015"
-            value={foundedYear}
-            onChange={(e) => setFoundedYear(e.target.value)}
-            min={1900}
-          />
-        </label>
-        <hr />
+          <label className="flex-col">
+            Founded Year
+            <input
+              className="block"
+              type="number"
+              name="foundedYear"
+              placeholder="2015"
+              value={foundedYear}
+              onChange={(e) => setFoundedYear(e.target.value)}
+              min={1900}
+            />
+          </label>
 
-        {renderOpeningHours()}
+          {renderOpeningHours()}
 
-        <button type="submit">Add</button>
-      </form>
+          <button className="flex mt-5 text-lg" type="submit">
+            Add
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
