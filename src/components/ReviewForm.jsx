e2bf0fb8e3001f338f5211a70fbfbfaffc5af4af
Casperavr <casper@hhc.earth>
2024-09-09 12:52:57 +0200

    style businessdetailpage and reviewform component

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -14,53 +14,59 @@ function ReviewForm({ getSpecificBusinessReviews }) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
         const requestBody = {title, text, rating, author: user._id, business: businessId};
-        console.log(requestBody)
-
         reviewService.createReview(businessId, requestBody)
             .then((response) => {navigate(`/businesses/${businessId}`)})
             .catch((error) => {console.log(error)});
-        
         getSpecificBusinessReviews();
+        setText("");
+        setTitle("");
+        setRating("");
     }
 
     return(
         <>
-            <form onSubmit={handleSubmit}>
-                {/* title text rating author business */}
-                <label>
-                    Title
-                        <input 
-                            type="text"
-                            name="title"
-                            placeholder="Bloody brilliant!"
-                            value={title}
-                            onChange={(e) => {setTitle(e.target.value)}}
-                            required
+            <form onSubmit={handleSubmit} className="w-full max-w-lg bg-white shadow-md rounded-lg p-6 mt-6">
+                <h3 className="text-xl font-semibold mb-4">Leave a Review</h3>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">
+                        Title
+                    </label>
+                    <input 
+                        type="text" 
+                        id="title"
+                        value={title}
+                        onChange={(e) => {setTitle(e.target.value)}}
+                        required
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         />
-                </label>
+                </div>
 
-                <label>
-                    Review
-                        <input 
-                            type="text"
-                            name="text"
-                            placeholder="Leave your review here."
-                            value={text}
-                            onChange={(e) => {setText(e.target.value)}}
-                            required
-                        />
-                </label>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="text">
+                        Review
+                    </label>
+                    <textarea 
+                        id="text"
+                        value={text}
+                        onChange={(e) => {setText(e.target.value)}}
+                        required
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    />
+                </div>
 
-                <label>
-                    Rating
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="rating">
+                        Rating
+                    </label>
                     <select 
+                        id="rating"
                         name="rating" 
                         value={rating}
                         onChange={(e) => {setRating(e.target.value)}}
                         required
-                    >
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        >
                         <option value="" disabled>
                             Select a rating
                         </option>
@@ -70,10 +76,16 @@ function ReviewForm({ getSpecificBusinessReviews }) {
                         <option value="4">4</option>
                         <option value="5">5</option>
                     </select>
-                </label>
+                </div>
 
-                <button type="submit">Submit rating</button>
-                <hr />
+                <div className="flex- items-center justify-between">
+                    <button 
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+                        type="submit"
+                    >
+                        Submit rating
+                    </button>
+                </div>
 
             </form>
         </>
