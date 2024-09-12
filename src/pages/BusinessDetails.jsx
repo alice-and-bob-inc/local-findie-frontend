8e5d9b1289a9fedc80f3ac73eead5144dbff08ea
Nicolas Anzoategui <nanzoate@gmail.com>
2024-09-13 00:02:57 +0200

    add condition to check if user is logged in before using user._id to render delete button

--- src/pages/BusinessDetails.jsx
+++ src/pages/BusinessDetails.jsx
@@ -251,17 +251,17 @@ function BusinessDetails () {
                                     <div key={review._id} className="bg-white shadow-md rounded-lg p-4 mb-4 w-full">
                                         <p className="text-lg font-semibold">{review.title}</p>
                                         <hr className="my-2"/>
-                                        {/* <p className="text-sm text-gray-600">Written by {user._id === review.author._id ? "you" : review.author.name}</p> */}
+                                        <p className="text-sm text-gray-600">Written by {user && user._id === review.author._id ? "you" : review.author.name}</p>
                                         <p className="mt-2 text-sm md:text-base">{review.text}</p>
                                         <p className="mt-2 text-sm md:text-base">{displayRating(review.rating)}</p>
-                                        {/* {user._id === review.author._id &&
+                                        {user && user._id === review.author._id &&
                                             <button 
                                                 onClick={() => {deleteReview(review._id, review.author._id)}}
                                                 className="card bg-red-500 hover:bg-red-700 text-white font-bold text-sm py-2 my-1 px-4 mx-1 rounded focus:shadow-outline"
                                             >
                                                 Delete
                                             </button>
-                                        } */}
+                                        }
                                     </div>
                                 )
                             }))
