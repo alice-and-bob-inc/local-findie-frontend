ac0a74dcbd2182e7b98974f0d810cbf4a739cc2f
Nicolas Anzoategui <nanzoate@gmail.com>
2024-09-12 16:36:26 +0200

    improve nav bar responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 function Footer() {
     return (
-      <footer className="bg-green-300 text-xs text-center p-1 fixed bottom-0 w-screen z-20 border border-t-green-400">
+      <footer className="bg-green-300 text-xs lg:text-sm text-center p-1 fixed bottom-0 w-screen z-20 border border-t-green-400">
         <a className="hover:text-white" href="https://github.com/alice-and-bob-inc/local-findie-frontend" target="_blank">
           View this project on GitHub
         </a>
